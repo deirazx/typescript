1ff81358e741b17de02ae308c1478d7d0d0e47a5
deirazx <deirazcodes@gmail.com>
2026-09-11 12:26:45 +0530

    Number data type

---
+++ number.ts
@@ -0,0 +1,34 @@
+let age: number = 25;
+// age = "Twenty Five"
+console.log(age);
+
+
+// Integar
+let marks: number = 90;
+console.log(marks);
+
+let percentage: number = 80.02;
+console.log(percentage);
+
+let temp: number = -0.2;
+console.log(temp);
+
+// NaN
+let result: number = 0/0;
+console.log(result);
+
+// Infinity
+let data: number = 10/0;
+console.log(data);
+
+// Add
+let a: number = 10;
+let b: number = 20;
+console.log(`a+b: ${a+b}, a-b: ${a-b}, a*b: ${a*b}, a/b: ${a/b}, a%b: ${a%b}`);
+
+
+function add(a: number, b: number) {
+  return a + b
+}
+
+console.log(add(10,20))
