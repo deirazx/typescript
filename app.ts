037eb822fb223d5d30605f72202df780abe78a62
deirazx <deirazcodes@gmail.com>
2026-09-11 12:18:42 +0530

    Variables & Basics Types

--- app.ts
+++ app.ts
@@ -1,4 +1,21 @@
 let username: string = "Dheeraj kumar"
 username = "Riya"
 // username = 900
-console.log(username);
+// console.log(username);
+
+
+// Number Type
+let price: number = 999.9;
+console.log(price)
+
+// String Type
+let username2: string = "Dheeraj"
+console.log(username2)
+
+//Boolean Type
+let isLoggedIn: boolean = true;
+console.log(isLoggedIn);
+
+
+let course = "Typescript"
+console.log(typeof course);
