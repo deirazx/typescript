48e619f529b4844cabba03ce91e942e4d094b02d
deirazx <deirazcodes@gmail.com>
2026-09-11 13:29:33 +0530

    Any & Unknown in TypeScript

---
+++ Any.ts
@@ -0,0 +1,15 @@
+// Any Type
+
+let data: any = "Dheeraj";
+data = 20;
+data = true;
+data = {
+  name: "Riya"
+}
+
+console.log(data);
+
+let value: any = "Hello";
+value = 123
+
+console.log(value)
---
+++ Unknown.ts
@@ -0,0 +1,7 @@
+let value: unknown = "Ts";
+
+console.log(typeof value)
+
+if (typeof value === "string") {
+  console.log(value)
+}
